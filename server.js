7bfc1804d78f8322a4510b434d85f8c82da167b7
Dehya Khurraim Siddiqui <57868646+DehyaKhurraim@users.noreply.github.com>
2023-07-07 23:48:32 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,96 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const PORT = process.env.PORT || 8080;
 const app = express();
-const port = 3000;
+app.use(express.json());
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    "mongodb+srv://admin:123@userregistration.7sk2dz2.mongodb.net/UserRegistration?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Node API app is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.post(
+  "/api/register",
+  [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("email").isEmail().withMessage("Invalid email").normalizeEmail(),
+    body("password")
+      .notEmpty()
+      .withMessage("Password is required")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  async (req, res) => {
+    console.log("Connected to React");
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Email is already registered" }] });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      user = new User({
+        name,
+        email,
+        password: hashedPassword,
+      });
+
+      await user.save();
+
+      res.status(200).json({
+        message: "User registered successfully",
+        user: {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+        },
+      });
+    } catch (error) {
+      console.error("Error registering user", error);
+      res.status(500).json({ message: "Server error" });
+    }
+  }
+);
